36dc0fbcbc2c26778be015754f869b977bc46ca9
admin <Chen_Yasin@163.com>
2020-01-10 13:51:36 +0800

    列表增删排序练习

---
+++ 练习文件集合/list_sort.py
@@ -0,0 +1,13 @@
+names = ['beijing','xian','chengdu','dali','Maldives']
+# print(sorted(names))
+# print(sorted(names, reverse=True))
+# print(names)
+# names.reverse()
+# print(names)
+# names.reverse()
+# # print(names)
+# names.sort()
+# print(names)
+names.sort(reverse=True)
+print(names)
+print(len(names))
--- 练习文件集合/list_test.py
+++ 练习文件集合/list_test.py
@@ -1,5 +1,28 @@
-names = ["trek","cannondale",'redline','specialized']
-print(names[1].title())
-print(names[0].upper())
-print(names[2].lower())
+# names = ['tom','jer','tark']
+#
+# print("Hi "+names.pop().title()+" may I have dinner?")
+# print("Hi "+names.pop().title()+" may I have dinner?")
+# print("Hi "+names.pop().title()+" may I have dinner?")
+#
+# names = ['tom','jer','tark']
+# del names[1]
+# names.append('micael')
+#
+# print("Hi "+names.pop().title()+" may I have dinner?")
+# print("Hi "+names.pop().title()+" may I have dinner?")
+# print("Hi "+names.pop().title()+" may I have dinner?")
 
+# names = ['tom','jer','tark']
+# names.append("lisi")
+# names.insert(0,'tyn')
+# names.insert(-0,'panda')
+# print(names)
+
+names = ['tom','jer','tark']
+names_del = names.pop()
+names.append('micael')
+
+print("Hi "+names.pop().title()+" may I have dinner?")
+print("Hi "+names.pop().title()+" may I have dinner?")
+print("Hi "+names.pop().title()+" may I have dinner?")
+print(names_del.title()+" Unfortunately!")
